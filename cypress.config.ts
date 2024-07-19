52d7291b71c465dc0643461860a48a2d8fba9736
“Kyle <kylemboomer@gmail.com>
2024-07-19 13:11:44 -0500

    Pass Initial Cypress Testing for MainPage Component

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
+  component: {
+    specPattern: "src/**/*.cy.{js,jsx,ts,tsx}",
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  }
 });
