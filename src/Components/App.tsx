c5f18230115f59181c8a4fb4801a84f15151c122
jordan williamson <williamsonjordan05@gmail.com>
2024-07-19 15:46:01 -0600

    Updated stlying for MainPage and Header component

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,46 +1,18 @@
 "use client"
 import '../CSS/App.css'
 import { useEffect, useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import MainGallery from './MainGallery'
 import MyGallery from './MyGallery'
-import ArtCard from './ArtCard'
-import Logo from '../Assets/Logo.png'
-import { Record, Image } from '../Utility/Types'
 import MainPage from './MainPage'
+import Header from './Header'
 
-
-// const video = require('../Assets/sampleVideo1.mp4');
 
 const App: React.FC = () => {
-  // const [allRecords, setAllRecords] = useState<Record[]>([]);
-
-  // useEffect(() => {
-  //   fetch('https://www.rijksmuseum.nl/api/en/collection?key=Ac7mP6Ke&technique=painting&ps=50')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       console.log(data.artObjects)
-  //       setAllRecords([data.artObjects])
-  //     })
-  //     .catch(error => console.error('Failed to fetch records', error))
-  // }, []);
-
-  // const handleFavorite = (id: number) => {
-  //   setAllRecords(allRecords.map(record =>
-  //     record.id === id ? { ...record, favorite: !record.favorite } : record
-  //   ));
-  // };
 
   return (
     <main className='AppContainer'>
-      <div className="Header">
-        <img src={Logo} className="main-logo" alt="Logo" />
-        <nav className="Links">
-          <Link to='/MainGallery' className="MainGallery-button">Main Gallery</Link>
-          <p className="big-slash">/</p>
-          <Link to='/MyGallery' className="MyGallery-button"> My Gallery</Link>
-        </nav>
-      </div>
+      <Header />
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route
