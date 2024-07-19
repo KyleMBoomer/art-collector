e2654eae2e382cae945f640ed122fd014888ea52
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-19 14:31:51 -0600

    adds linked info

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -9,17 +9,27 @@ const Footer: React.FC = () => {
             <p className='created-by'>Created by:</p>
             <div className='info-section'>
                 <h2 className='name'>Jordan Williamson</h2>
-                    <img className='github' src={github} alt='github-logo'/>
+                    <img className='github' src={github} alt='GitHub-logo'/>
                     <a href='https://github.com/JWill06' className='jordan-github'>@GitHub</a>
+                    <img className='linkedin' src={linkedin} alt='LinkedIn-Logo'/>
+                    <a href='https://www.linkedin.com/in/jordan-williamson-a079b3271' className='jordan-linkedin'> @LinkedIn</a>
                 <h2 className='name'>Kim Ewing</h2>
-                    <img className='github' src={github} alt='github-logo'/>
+                    <img className='github' src={github} alt='GitHub-logo'/>
                     <a href= 'https://github.com/kiewi16' className='kim-github'>@GitHub</a>
+                    <img className='linkedin' src={linkedin} alt='LinkedIn-Logo'/>
+                    <a href='https://www.linkedin.com/kimberlypekar' className="kim-linkedin"> @LinkedIn</a>
                 <h2 className='name'>Kyle Boomer</h2>
-                    <img className='github' src={github} alt='github-logo'/>
+                    <img className='github' src={github} alt='GitHub-logo'/>
                     <a href='https://github.com/kylemboomer' className='kyle-github'>@GitHub</a>
-                <h2 className='name'>Seong Kang</h2>
-                    <img className='github' src={github} alt='github-logo'/>
-                    <a href='https://github.com/Sanghoro' className='seong-github'>@GitHub</a>
+                    <img className='linkedin' src={linkedin} alt='LinkedIn-Logo'/>
+                    <a href='https://www.linkedin.com/in/kylemboomer/' className="kyle-linkein"> @LinkedIn</a>
+                <div className='seong-kang'>
+                    <h2 className='name'>Seong Kang</h2>
+                        <img className='github' src={github} alt='GitHub-logo'/>
+                        <a href='https://github.com/Sanghoro' className='seong-github'>@GitHub</a>
+                        <img className='linkedin' src={linkedin} alt='LinkedIn-Logo'/>
+                        <a href='https://www.linkedin.com/in/seong-kang/' className="seong-linkedin"> @LinkedIn</a>
+                </div>
             </div>
         </footer>
     )
