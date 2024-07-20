89cf09583c9cb25ea5d151e0d9416b7a87f4bc44
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-20 15:05:50 -0600

    adds handleFavorite function

--- src/Components/MainGallery.tsx
+++ src/Components/MainGallery.tsx
@@ -3,20 +3,32 @@ import '../CSS/MainGallery.css'
 import ArtCard from './ArtCard'
 import { Record, Image } from '../Utility/Types'
 import { useEffect, useState } from 'react'
+import MyGallery from './MyGallery'
+import { useFavorites } from './Favorites'
 
 interface MainGalleryProps {
     records: Record[];
-    // handleFavorite: (id: number) => void;
+    handleFavorite: (record) => void;
 }
 
 const MainGallery: React.FC = () => {
     const [allRecords, setAllRecords] = useState<Record[]>([]);
+    const [favoriteRecords, setFavoriteRecords] = useFavorites<Record[]>([]);
+
+    function handleFavorite(record) {
+        const isAlreadyFavorited = favoriteRecords.some(favoriteRecord => favoriteRecord.id === record.id)
+
+        if (isAlreadyFavorited) {
+            return setFavoriteRecords(favoriteRecords.filter(favoriteRecord => favoriteRecord.id !== record.id))
+        } else {
+            setFavoriteRecords([...favoriteRecords, record])    
+        }
+    }
 
     useEffect(() => {
         fetch('https://www.rijksmuseum.nl/api/en/collection?key=Ac7mP6Ke&technique=painting&ps=25')
             .then(response => response.json())
             .then(data => {
-                console.log(data.artObjects)
                 setAllRecords([...data.artObjects])
             })
             .catch(error => console.error('Failed to fetch records', error))
@@ -26,7 +38,8 @@ const MainGallery: React.FC = () => {
         return (
             <div key={record.id}>
                 <ArtCard
-                    record={record}
+                record={record}
+                handleFavorite={handleFavorite}
                 />
             </div>
         )
@@ -36,6 +49,7 @@ const MainGallery: React.FC = () => {
         <div className='main-gallery'>
             <h2 className="MainGallery-Title">Main Gallery</h2>
             {artCards}
+            {/* <MyGallery favoriteRecords={favoriteRecords} /> */}
         </div>
     )
 }
