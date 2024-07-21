f65072e8568b4146ce1d2232436b0ac64469b9c6
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-20 22:35:35 -0600

    updates TypeScript

--- src/Components/Favorites.tsx
+++ src/Components/Favorites.tsx
@@ -1,16 +1,22 @@
 import { createContext, useContext, useState } from 'react';
 import { FavoriteRecord } from '../Utility/Types'
 
-const FavoriteContext = createContext<[FavoriteRecord[], (favoriteRecords: FavoriteRecord[]) => void]>()
+const defaultValue: [FavoriteRecord[], (favoriteRecords: FavoriteRecord[]) => void] = [
+  [],
+  () => {}
+]
+
+const FavoriteContext = createContext(defaultValue)
 
 export const useFavorites = () => useContext(FavoriteContext)
 
 export const FavoriteProvider = ({ children }: { children: React.ReactNode }) => {
-  const [favoriteRecords, setFavoriteRecords] = useState<FavoriteRecord[]>([])
+  const [favoriteRecords, setFavoriteRecords] = useState<FavoriteRecord[]>([]);
   
   return (
     <FavoriteContext.Provider value={[favoriteRecords, setFavoriteRecords]}>
       {children}
     </FavoriteContext.Provider>
-  )  
+  )
+  
 }
