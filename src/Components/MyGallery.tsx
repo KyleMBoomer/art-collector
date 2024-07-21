5554ab07be07bc17315e402aadc41f44e9b459b0
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-20 21:55:28 -0600

    changes instances of record to favoriteRecord, maps over favoriteRecords to create FavoriteArtCard components, and returns favorideArtCards

--- src/Components/MyGallery.tsx
+++ src/Components/MyGallery.tsx
@@ -2,16 +2,24 @@ import React from 'react'
 import '../CSS/MyGallery.css'
 import ArtCard from './ArtCard'
 import { useFavorites } from './Favorites'
+import FavoriteArtCard from './FavoriteArtCard'
 
 const MyGallery: React.FC = () => {
     const [favoriteRecords] = useFavorites()
 
+    const favoriteArtCards = favoriteRecords.map(favoriteRecord => {
+        return (
+            <FavoriteArtCard 
+            key={favoriteRecord.id} 
+            favoriteRecord={favoriteRecord}
+            />
+        )   
+    })
     return (
-        <div className="my-gallery">
-            <h2>Favorites</h2>
-            {favoriteRecords.map((record) => (
-                <ArtCard key={record.id} record={record} />
-            ))}
+        <div className='my-gallery'>
+            <h2 className="MyGallery-Title">My Gallery</h2>
+            {favoriteArtCards}
+            {/* <MyGallery favoriteRecords={favoriteRecords} /> */}
         </div>
     )
 }
