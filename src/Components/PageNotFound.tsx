51f2084ee1b8b7d18047e67ec2c91ff9196c0c70
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-20 19:57:22 -0600

    changes className of div

--- src/Components/PageNotFound.tsx
+++ src/Components/PageNotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function PageNotFound() {
     return (
-        <div className="not-found">
+        <div className="page-not-found">
             <h1>404-Page Not Found!</h1>
             <p>How did you get here????</p>
             <Link to="/">Go back to the homepage!</Link>
