47b4da83565d9a9d329a25520aa7ae31918f3a01
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-20 21:48:19 -0600

    adds TypeScript

--- src/Components/Favorites.tsx
+++ src/Components/Favorites.tsx
@@ -1,16 +1,16 @@
-import React, { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { FavoriteRecord } from '../Utility/Types'
 
-const FavoriteContext = createContext()
+const FavoriteContext = createContext<[FavoriteRecord[], (favoriteRecords: FavoriteRecord[]) => void]>()
 
 export const useFavorites = () => useContext(FavoriteContext)
 
-export const FavoriteProvider = ({ children }) => {
-  const [favoriteRecords, setFavoriteRecords] = useState([])
+export const FavoriteProvider = ({ children }: { children: React.ReactNode }) => {
+  const [favoriteRecords, setFavoriteRecords] = useState<FavoriteRecord[]>([])
   
   return (
     <FavoriteContext.Provider value={[favoriteRecords, setFavoriteRecords]}>
       {children}
     </FavoriteContext.Provider>
-  )
-  
+  )  
 }
