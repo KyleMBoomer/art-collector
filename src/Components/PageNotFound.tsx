8deb6e0c93e3bbdd7bdc6f6b684d5e51c5f4c3a3
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-20 19:54:36 -0600

    adds PageNotFound component that will render when the user tries to access a URL that doesn't exist or has changed

---
+++ src/Components/PageNotFound.tsx
@@ -0,0 +1,14 @@
+import '../CSS/PageNotFound.css'
+import { Link } from "react-router-dom";
+
+function PageNotFound() {
+    return (
+        <div className="not-found">
+            <h1>404-Page Not Found!</h1>
+            <p>How did you get here????</p>
+            <Link to="/">Go back to the homepage!</Link>
+        </div>
+    )
+}
+
+export default PageNotFound
