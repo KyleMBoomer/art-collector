a5e4df54c531bd223afd7e0c9c4838eb551ec212
Seong <seong.h.kang2@gmail.com>
2024-07-22 15:19:15 -0500

    Update spec_MainGallery.cy.ts

--- cypress/e2e/spec_MainGallery.cy.ts
+++ cypress/e2e/spec_MainGallery.cy.ts
@@ -1,24 +1,23 @@
 describe('Main Gallery component', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=Ac7mP6Ke&technique=painting&ps=25',
-      { fixture: 'records.json' }
-    ).as('getRecords');
-    cy.visit('http://localhost:3000/');
-  });
+    cy.intercept('GET', 'https://www.rijksmuseum.nl/api/en/collection?key=Ac7mP6Ke&technique=painting&ps=25', 
+      {fixture: 'records.json'}
+     ).as('getRecords')
+    cy.visit('http://localhost:3000/')
+  })
 
   it('should navigate to Main Gallery when link is clicked', () => {
     cy.get('.MainGallery-button').click();
     cy.url().should('include', '/MainGallery');
     cy.get('.MainGallery-Title').should('contain', 'Main Gallery');
-  });
+  })
 
   it('should display the main gallery with art cards', () => {
-    cy.get('.MainGallery-button').click();
-    cy.url().should('include', '/MainGallery');
+    cy.visit('http://localhost:3000/MainGallery');
     cy.wait('@getRecords');
     cy.get('.main-gallery').should('exist');
     cy.get('.MainGallery-Title').should('contain', 'Main Gallery');
-    cy.get('.card').should('have.length', 2)
+    cy.get('.art-card').should('have.length', 2); 
   });
 
   it('should handle fetch errors gracefully', () => {
@@ -26,9 +25,8 @@ describe('Main Gallery component', () => {
       statusCode: 500
     }).as('getArtRecordsError');
 
-    cy.get('.MainGallery-button').click();
-    cy.url().should('include', '/MainGallery');
+    cy.visit('http://localhost:3000/MainGallery');
     cy.wait('@getArtRecordsError');
     cy.get('.main-gallery').should('contain', 'Failed to fetch records');
-  });
-});
+  })
+})
